eb7a61bc7ddc54b73e4bd42277be019047bac67b
rr9919-cod <rohithmartin@gmail.com>
2026-09-07 08:44:10 +0530

    Implement PalindromeChecker with multiple methods

---
+++ Week-01/PalindromeChecker.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class PalindromeChecker {
+
+    static boolean isPalindromeIterative(String text) {
+
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+
+            if (text.charAt(left) != text.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    static boolean isPalindromeRecursive(String text) {
+
+        if (text.length() <= 1)
+            return true;
+
+        if (text.charAt(0) != text.charAt(text.length() - 1))
+            return false;
+
+        return isPalindromeRecursive(text.substring(1, text.length() - 1));
+    }
+
+    static boolean isPalindromeArrayReversal(String text) {
+
+        char[] original = text.toCharArray();
+        char[] reverse = text.toCharArray();
+
+        for (int i = 0; i < reverse.length / 2; i++) {
+
+            char temp = reverse[i];
+            reverse[i] = reverse[reverse.length - 1 - i];
+            reverse[reverse.length - 1 - i] = temp;
+        }
+
+        return Arrays.equals(original, reverse);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String text = sc.nextLine();
+
+        System.out.println();
+
+        System.out.println("Iterative: " +
+                (isPalindromeIterative(text) ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Recursive: " +
+                (isPalindromeRecursive(text) ? "Palindrome" : "Not Palindrome"));
+
+        System.out.println("Array Reversal: " +
+                (isPalindromeArrayReversal(text) ? "Palindrome" : "Not Palindrome"));
+
+        sc.close();
+    }
+}
