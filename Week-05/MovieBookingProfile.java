7219352980e2cc126ef539df72251db462209cb5
rr9919-cod <rohithmartin@gmail.com>
2026-09-07 10:56:55 +0530

    Add MovieBookingProfile class for booking management

---
+++ Week-05/MovieBookingProfile.java
@@ -0,0 +1,35 @@
+class MovieBookingProfile {
+
+    private String name;
+    private boolean confirmed;
+    private String otp;
+
+    public MovieBookingProfile() {
+        this.confirmed = false;
+    }
+
+    public MovieBookingProfile(String name) {
+        this();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+}
